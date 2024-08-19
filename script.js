979dd8357caf059e9056de277e71d208d303ed90
sunny <dharmindersingh.gne@gmail.com>
2024-08-19 19:53:36 +0100

    tweet() function defined

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const api_url = "https://api.quotable.io/random";
+const quote = document.querySelector('#quote');
+const author = document.querySelector('#author');
+
 
 async function getQuote(url) {
   let response = await fetch(url);
   let data = await response.json();
-  console.log(data);
+
+  quote.innerHTML = data.content;
+  author.innerHTML = data.author;
+  
 }
 
 getQuote(api_url);
+
+function tweet() {
+  window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + '----by ' + author.innerHTML, "tweet window", "width=600px height 300px");
+}
+
+
+
+
+
